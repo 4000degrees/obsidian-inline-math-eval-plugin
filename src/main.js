24664488021e58408cc82cb17e2582dac9e44ca7
Anthony R <4000degrees@gmail.com>
2021-05-07 18:33:00 +0300

    now it doesn't remove everuthing after trigger

--- src/main.js
+++ src/main.js
@@ -10,28 +10,21 @@ var obsidian = require('obsidian');
 export default class InlineMathEval extends obsidian.Plugin {
   constructor() {
     super(...arguments);
-
-
     const config = {}
     this.math = create(all, config)
 
-    // Line Edit Changes
     this.codemirrorLineChanges = (cm, change) => {
       this.check_lines(cm, change.from.line, change.from.line + change.text.length - 1);
     };
 
-    // Check Single Line
     this.check_line = (cm, line_number, targetFile) => {
-
-      const regex = /(.*)::=/;
       const line = cm.lineInfo(line_number);
       if (line === null)
         return;
 
+      const regex = /(.*)::=/;
       const match = line.text.match(regex);
-
       if (match) {
-
         var result = false;
         var triggerIndex = line.text.indexOf("::=")
         var maybeSpace = line.text.charAt(triggerIndex - 1) == " " ? "" : " "
@@ -42,14 +35,13 @@ export default class InlineMathEval extends obsidian.Plugin {
           new obsidian.Notice(e);
           result = ""
         }
-
         cm.replaceRange(result, {
           line: line_number,
           ch: triggerIndex
         }, {
-          line: line_number
+          line: line_number,
+          ch: triggerIndex + 3
         })
-
       }
     };
 
